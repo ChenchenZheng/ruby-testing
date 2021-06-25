410fd2307d527b2df13a76f6c9e9c8980b42276c
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-06-25 09:47:35 +0200

    ✅ Allow todos to be completed spec

--- 02_test_driven_rails/todos/db/schema.rb
+++ 02_test_driven_rails/todos/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_134151) do
+ActiveRecord::Schema.define(version: 2021_06_25_073923) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.datetime "completed_at"
   end
 
 end
