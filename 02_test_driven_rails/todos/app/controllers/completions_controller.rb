cb906fbc9629193fa16940707555740b9e86e46d
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-06-25 10:39:44 +0200

    ♻️ Refactor tests to use helper methods

--- 02_test_driven_rails/todos/app/controllers/completions_controller.rb
+++ 02_test_driven_rails/todos/app/controllers/completions_controller.rb
@@ -1,7 +1,13 @@
 class CompletionsController < ApplicationController
   def create
     # touch, update the specified timestamps of the model with the current time
-    current_user.todos.find(params[:todo_id]).touch :completed_at
+    todo.touch :completed_at
     redirect_to todos_path
+  end
+
+  private
+
+  def todo
+    current_user.todos.find(params[:todo_id])
   end
 end
