5715c8b64f17a8d5e23201545362c8b919e3ab79
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-06-26 12:39:04 +0200

    ✅ Add and pass tests

--- 03_Rails_testing_exercises/testing-fundamentals-testing-activerecord/spec/models/person_spec.rb
+++ 03_Rails_testing_exercises/testing-fundamentals-testing-activerecord/spec/models/person_spec.rb
@@ -1,9 +1,22 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Person do
-  it "requires a name to be valid" do
-    # Write your spec here.
-    # You can delete the pending line.
-    pending "Not written yet"
+  it 'is invalid without a first name' do
+    chenchen = Person.new(first_name: nil)
+
+    expect(chenchen).to be_invalid
+  end
+
+  it 'is invalid with a blank first name' do
+    chenchen = Person.new(first_name: '')
+
+    expect(chenchen).to be_invalid
+  end
+
+  it 'is valid with a first name' do
+    chenchen = Person.new(first_name: 'Chenchen')
+
+    expect(chenchen).to be_valid
+    # my version -> expect(chenchen.valid?).to eq(true)
   end
 end
