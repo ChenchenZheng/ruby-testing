6193338fe5eefa5e43171e0c581872eae02d2316
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-06-26 17:11:46 +0200

    ✅ Simple integration test done

--- 03_Rails_testing_exercises/testing-fundamentals-write-an-integration-test/spec/features/user_creates_person_spec.rb
+++ 03_Rails_testing_exercises/testing-fundamentals-write-an-integration-test/spec/features/user_creates_person_spec.rb
@@ -1,7 +1,22 @@
-require "rails_helper"
+require 'rails_helper'
 
-feature "user creates person" do
-  scenario "with valid data" do
-    pending "Use Capybara to create a new person"
+feature 'user creates person' do
+  scenario 'with valid data' do
+    visit new_person_path
+
+    fill_in 'First name',	with: 'Chenchen'
+    click_on 'Create Person'
+
+    expect(page).to have_content("Person created.")
+    expect(page).to have_content('Chenchen')
+  end
+
+  scenario 'without valid data' do
+    visit new_person_path
+
+    fill_in 'First name',	with: nil
+    click_on 'Create Person'
+
+    expect(page).not_to have_content('Chenchen')
   end
 end
