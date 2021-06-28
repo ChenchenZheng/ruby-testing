5940700712c7321099b0dc1a6524cb498a618250
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-06-28 16:21:04 +0200

    ✅ Version without stubs

--- 03_Rails_testing_exercises/testing-fundamentals-write-a-controller-spec/spec/controllers/people_controller_spec.rb
+++ 03_Rails_testing_exercises/testing-fundamentals-write-a-controller-spec/spec/controllers/people_controller_spec.rb
@@ -1,38 +1,32 @@
 require 'rails_helper'
 
-describe PeopleController do
+describe PeopleController, type: :controller do
+  let(:attributes) do
+    {
+      first_name: 'Chenchen'
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      first_name: nil
+    }
+  end
+
   describe '#create' do
     context 'when person is valid' do
       it 'redirects to #show' do
-        person = stub_valid_person
+        post :create, params: { person: attributes }
 
-        post :create, person: { first_name: 'John' }
-
-        expect(response).to redirect_to(person)
+        expect(response).to redirect_to(Person.last)
       end
     end
 
     context 'when person is invalid' do
       it 'renders new' do
-        person = stub_invalid_person
-
-        post :create, person: { first_name: 'John' }
+        post :create, params: { person: invalid_attributes }
 
         expect(response).to render_template(:new)
-      end
-    end
-
-    def stub_valid_person
-      Person.new.tap do |person|
-        allow(Person).to receive(:new).and_return person
-        allow(person).to receive(:save).and_return(true)
-      end
-    end
-
-    def stub_invalid_person
-      Person.new.tap do |person|
-        allow(Person).to receive(:new).and_return person
-        allow(person).to receive(:save).and_return(false)
       end
     end
   end
