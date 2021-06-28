60fa0426702c004f9889f4f753b4298bfdd22f63
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-06-28 13:47:37 +0200

    🧪 Controller spec with stubs

--- 03_Rails_testing_exercises/testing-fundamentals-write-a-controller-spec/spec/controllers/people_controller_spec.rb
+++ 03_Rails_testing_exercises/testing-fundamentals-write-a-controller-spec/spec/controllers/people_controller_spec.rb
@@ -1,16 +1,38 @@
-require "rails_helper"
+require 'rails_helper'
 
 describe PeopleController do
-  describe "#create" do
-    context "when person is valid" do
-      it "redirects to #show" do
-        pending "create this test"
+  describe '#create' do
+    context 'when person is valid' do
+      it 'redirects to #show' do
+        person = stub_valid_person
+
+        post :create, person: { first_name: 'John' }
+
+        expect(response).to redirect_to(person)
       end
     end
 
-    context "when person is invalid" do
-      it "renders the 'new' template" do
-        pending "create this test"
+    context 'when person is invalid' do
+      it 'renders new' do
+        person = stub_invalid_person
+
+        post :create, person: { first_name: 'John' }
+
+        expect(response).to render_template(:new)
+      end
+    end
+
+    def stub_valid_person
+      Person.new.tap do |person|
+        allow(Person).to receive(:new).and_return person
+        allow(person).to receive(:save).and_return(true)
+      end
+    end
+
+    def stub_invalid_person
+      Person.new.tap do |person|
+        allow(Person).to receive(:new).and_return person
+        allow(person).to receive(:save).and_return(false)
       end
     end
   end
